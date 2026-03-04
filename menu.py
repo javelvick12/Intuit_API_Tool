b12575d44fd7efcf3e14099cf5811c0beebeb9fb
jerome <jmalthoff@yahoo.com>
2026-03-04 09:38:31 -0700

    started thinking about the menu

--- menu.py
+++ menu.py
@@ -6,7 +6,33 @@ import time
 
 ########### Deliverable ###########
 
+def menu_main():
+    """
+    Main display, session maintenance, timeout, exit, and loop logic
 
+    Args:
+
+    Return:
+
+    """
+    pass
+
+def menu_sub_1():
+    pass
+
+def menu_sub_2():
+    pass
+
+def menu_exit():
+    """
+    Cleanly exits menu upon user "exit/#" or timeout.
+
+    Args:
+
+    Returns:
+
+    """
+    pass
 
 ########### Main ###########
 def main():
